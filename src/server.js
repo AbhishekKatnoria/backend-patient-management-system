fe7af0c9768773fb3f3213cd06f64eda058ed81f
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2025-04-02 00:45:53 +0530

    Sync the DB

--- src/server.js
+++ src/server.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
-require("dotenv").config();
-
+const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
+const sequelize = require("./config/db");
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database synced successfully");
+  })
+  .catch((err) => {
+    console.error("Error syncing database:", err);
+  });
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors());
 
 app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
