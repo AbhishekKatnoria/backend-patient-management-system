e153e1e594ffe3fe9a41b4f22edab1e00223fe3b
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2025-04-02 00:47:09 +0530

    Added the auth middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 
-const verifyToken = (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) return res.status(401).json({ error: "Unauthorized" });
+module.exports = (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Access Denied" });
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.status(403).json({ error: "Invalid token" });
+  try {
+    const decoded = jwt.verify(token, "secretKey");
     req.user = decoded;
     next();
-  });
+  } catch (error) {
+    res.status(400).json({ message: "Invalid Token" });
+  }
 };
-
-module.exports = { verifyToken };
