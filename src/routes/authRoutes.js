1f5d08543592253f748e7e113e232e339bd20fb7
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2025-04-02 00:46:27 +0530

    Changed the auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const { registerSuperAdmin, loginSuperAdmin, logoutSuperAdmin, getSuperAdminProfile } = require("../controllers/authController");
-const { verifyToken } = require("../middleware/authMiddleware");
-
 const router = express.Router();
+const authController = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/register", registerSuperAdmin);
-router.post("/login", loginSuperAdmin);
-router.post("/logout", logoutSuperAdmin);
-router.get("/profile", verifyToken, getSuperAdminProfile);
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.get("/profile", authMiddleware, authController.getProfile);
+router.post("/logout", authController.logout);
 
 module.exports = router;
