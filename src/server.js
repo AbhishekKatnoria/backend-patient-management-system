d2c474eadef9c401eb6bb81927b419c34793fa48
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2025-04-05 23:02:04 +0530

    Fixed the server

--- src/server.js
+++ src/server.js
@@ -4,22 +4,24 @@ const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
 const sequelize = require("./config/db");
 
-sequelize
-  .sync()
-  .then(() => {
-    console.log("Database synced successfully");
-  })
-  .catch((err) => {
-    console.error("Error syncing database:", err);
-  });
-
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-app.use(cors());
 
+// Middlewares
+app.use(cors());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Routes
 app.use("/auth", authRoutes);
 
+// DB Connection
+sequelize
+  .sync({force: true})
+  .then(() => console.log("Database synced successfully"))
+  .catch((err) => console.error("Error syncing database:", err));
+
+// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
